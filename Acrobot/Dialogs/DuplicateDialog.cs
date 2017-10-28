b3726d9dad944d438fb4905b7bbf5cac482c46ec
Peter O'Connor <poc275@gmail.com>
2017-10-28 16:15:03 +0100

    Telemetry bug fix and duplicate acronyms feature added

---
+++ Acrobot/Dialogs/DuplicateDialog.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Acrobot.Dialogs
+{
+    public class DuplicateDialog : IDialog<string>
+    {
+        int id;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.UserData.TryGetValue<int>("Id", out id);
+            TagDuplicateAcronym(id);
+            await context.PostAsync("Thankyou. This will help me provide better results");
+            context.Done("");
+        }
+
+
+        // function that tags the acronym as duplicate
+        private void TagDuplicateAcronym(int id)
+        {
+            Models.AcronymDBEntities db = new Models.AcronymDBEntities();
+
+            var query = (from Acronyms in db.Acronyms
+                         where Acronyms.Id == id
+                         select Acronyms);
+
+            foreach (var definition in query)
+            {
+                definition.Duplicate = true;
+            }
+
+            db.SaveChanges();
+        }
+    }
+}
--- Acrobot/Models/Acronym.cs
+++ Acrobot/Models/Acronym.cs
@@ -17,5 +17,6 @@ namespace Acrobot.Models
         public int Id { get; set; }
         public string Acronym1 { get; set; }
         public string Definition { get; set; }
+        public Nullable<bool> Duplicate { get; set; }
     }
 }
